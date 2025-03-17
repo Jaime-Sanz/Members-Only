f1c79b5efb075ea45763a74e4a698c48543c0447
Jaime Sanchez <118962371+Jaime-Sanz@users.noreply.github.com>
2025-03-17 15:29:42 -0700

    Initial project setup with Express, Passport, and session config

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import session from 'express-session';
+import dotenv from 'dotenv';
+import path from 'path';
+import {fileURLToPath} from 'url';
+import passport from './config/passport.js';
+
+dotenv.config()
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(session({secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use();
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Internal Server Error!' });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Listening on ${process.env.PORT || 3000}`);
+});
